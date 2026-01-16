b1352a48476fea2815b21ee0357f6f4fd89c923b
Simonhamel1 <156895661+Simonhamel1@users.noreply.github.com>
2026-01-16 14:33:06 +0100

    Réorganisation de la classe test : ajout de méthodes pour les opérations de base et affichage des résultats

--- test.java
+++ test.java
@@ -1,21 +1,36 @@
-class ManipulationVariables {
+class test{
+
    
-   public static void main(String[] args) {
-      int epargne = 500;
-      int revenus = 2000;
-      //Ajoutez 100 à votre épargne (Yeah!)
-      epargne = epargne + 100;
-
-      //Enlevez 50 à votre indemnité (Snif)
-      revenus = revenus - 50;
-      //Faites une mise à jour sur votre délai d'épargne
-      int nombreDeJoursEpargne = (5000 - revenus) / 500;
-    
-      //Mettez à jour à nouveau votre indemnité (encore)
-      revenus = revenus + (30 - 10) * 7;
-
-      System.out.println("Votre épargne est de : " + epargne + " euros");
-      System.out.println("Votre revenu est de : " + revenus + " euros");
-      System.out.println("Il vous reste " + nombreDeJoursEpargne + " jours pour atteindre votre objectif d'épargne.");
+   public void sampleMethod() {
+      System.out.println("This is a sample method.");
+      int x = 10;
+      int y = 20;
+      float z = 5.5f;
+      System.out.println("Sum: " + (x + y));
+      System.out.println("Multiplication: " + (x * y));
+      System.out.println("End of sample method.");
+      System.out.println("Value of z * x + y : " + (z * x + y));
+      int value = (int)(z*x+y);
+      System.out.println("Value of int z * x + y : " + value );
    }
+
+   public void unusedMethod(int a , float b) {
+      double c = a + b;
+      System.out.println("This method is not used anywhere. Value of c : " + c);
+   }
+
+   public void affichage_nom(String Prenom, String nom) {
+      System.out.println("Bonjour " + Prenom + " " + nom + "!");
+   }
+
+   public static void main(String[] args) {
+      System.out.println("test java");
+      test obj = new test();
+      obj.sampleMethod();
+      obj.unusedMethod(5, 3.2f);
+      String Prenom = "John";
+      String nom = "Doe";
+      obj.affichage_nom(Prenom, nom);
+   }
+
 }
